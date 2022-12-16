f2947d7003050e33c94acd126181eac3f5b0a624
Kirigaya-K <rithshy@gmail.com>
2022-12-16 11:21:31 +0100

    page coach

---
+++ SelectionEquipe_Dev/Coach.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SelectionEquipe_Dev
+{
+    partial class Coach
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.NPJoueur = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.NJ_coach = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PJ_coach = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(71, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mercato";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(733, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Profil coach";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(71, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Joueurs de l\'équipe";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NPJoueur});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(37, 159);
+            this.listView1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(297, 424);
+            this.listView1.TabIndex = 3;
+            this.listView1.TileSize = new System.Drawing.Size(15, 15);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // NPJoueur
+            // 
+            this.NPJoueur.Text = "Nom et Prenom";
+            this.NPJoueur.Width = 298;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(618, 118);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ma Selection";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NJ_coach,
+            this.PJ_coach});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(569, 159);
+            this.listView2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(297, 424);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // NJ_coach
+            // 
+            this.NJ_coach.Text = "Nom";
+            this.NJ_coach.Width = 131;
+            // 
+            // PJ_coach
+            // 
+            this.PJ_coach.Text = "Prenom";
+            this.PJ_coach.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PJ_coach.Width = 175;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 258);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 450);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 42);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Retirer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(906, 628);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 42);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Démission";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(934, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 37);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(906, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 41);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "default";
+            // 
+            // Coach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1052, 699);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Name = "Coach";
+            this.Text = "Coach";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader NJ_coach;
+        private System.Windows.Forms.ColumnHeader PJ_coach;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColumnHeader NPJoueur;
+    }
+}
+
---
+++ SelectionEquipe_Dev/Coach.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectionEquipe_Dev
+{
+    public partial class Coach : Form
+    {
+        public Coach()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = listView2.SelectedItems[0].Text;
+
+            listView1.Items.Add(item);
+
+            // item.Text = listView2.SelectedItems.Cast<ListViewItem>().First().Text; 
+
+            /*if (listView1.Items.Count == 1)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = listView2.SelectedItems[0].Text;
+
+                if (listView1.Items != listView2.Items)
+                {
+                    listView1.Items.Add(item);          //listView1.Items.RemoveAt(sel_index);
+                }
+                else
+                {
+                    MessageBox.Show("Desoler le joueur à déjà été placer !");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Séléctionner le joueur à ajouter");
+            }*/
+
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = textBox1.Text;
+            item.SubItems.Add(textBox1.Text);
+
+            listView2.Items.Add(item);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(listView1.SelectedIndices.Count == 1) 
+            {
+                int sel_index = listView1.SelectedIndices[0];
+                listView1.Items.RemoveAt(sel_index);
+            }
+            else
+            {
+                MessageBox.Show("Sélectionner le joueur a supprimer !");
+            }
+           
+        }
+    }
+}
